888458bf154ef5d43de92a14b9e3bf941adec819
Jacques Thurling <jthurling78@gmail.com>
2022-05-30 12:05:11 +0200

    Update: IsPositive Generics

---
+++ Lib/RealNumFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Lib;
+
+internal class RealNumFunctions<T> : IEquatable<T>
+{
+    public static bool IsPositive<T>(T type)
+    {
+        string num = type.ToString();
+        if (IsNum(type))
+        {
+            var value = ReturnNumValue(num, typeof(T));
+            return Comparer.Default.Compare(value, 0) < 0;
+        }
+
+        return false;
+    }
+
+    private static object ReturnNumValue(string? num, Type type)
+    {
+        if (type.GetType() is int)
+        {
+            return Int32.Parse(num);
+        }
+
+        if (type.GetType() is float)
+        {
+            return float.Parse(num);
+        }
+
+        return 0;
+    }
+
+    private static bool IsNum(object num)
+    {
+        Regex isNum = new Regex("^[0-9]+$");
+        return isNum.Match((string) num).Success;
+    }
+
+    public bool Equals(T? other)
+    {
+        throw new NotImplementedException();
+    }
+}
